3c718d4d5fe48e729713e196c80f1c6dbf40475b
maximilianosulsenti <maximilianosulsenti@gmail.com>
2025-10-10 22:36:11 -0300

    se crea navbar con reactbootstrap

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,31 +1,38 @@
-import "./NavBar.css"
-import CartWidget from "../CartWidget/CartWidget"
-import { Link} from "react-router-dom"
+import "./NavBar.css";
+import CartWidget from "../CartWidget/CartWidget";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavBar = () => {
-
   return (
-     <header>
-      <Link to="/">
-        <img className="logo" src="/Ondheros-img/ondheroslogo.jpg" alt="Logo de la tienda" />
-      </Link>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/categoria/remeras">Remeras</Link>
-            </li>
-            <li>
-              <Link to="/categoria/billeteras">Billeteras</Link>
-            </li>
-            <li>
-              <Link to="/categoria/medias">Medias</Link>
-            </li>
-          </ul>
-        </nav>
-   
-      <CartWidget/>
-     </header>     
-     )
-}
+    <Navbar expand="lg" variant="dark" className="navbar">
+      <Container className="nav-container">
+        <div>
+        <Navbar.Brand as={Link} to="/">
+          <img className="logo" src="/Ondheros-img/ondheroslogo.jpg" alt="Logo de la tienda" />
+        </Navbar.Brand>
+        </div>
 
-export default NavBar
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+       <div>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto nav-link">
+            <Nav.Link as={Link} to="/categoria/remeras">Remeras</Nav.Link>
+            <Nav.Link as={Link} to="/categoria/billeteras">Billeteras</Nav.Link>
+            <Nav.Link as={Link} to="/categoria/medias">Medias</Nav.Link>
+               <div className="cart-mobile">
+                <CartWidget />
+              </div>
+          </Nav>
+        </Navbar.Collapse>
+        </div>
+          <div className="cart-desktop">
+          <CartWidget />
+         </div>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
